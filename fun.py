88824ca4f4e9494cc7954994098f399b41e516a8
6bytes <35638771+SirKtus@users.noreply.github.com>
2020-03-26 12:20:51 +0100

    Create fun.py

---
+++ fun.py
@@ -0,0 +1,43 @@
+import discord
+import requests as r
+import random
+from discord.ext import commands
+
+class fun(commands.Cog):
+    def __init__(self, client):
+        self.bot = client
+
+    @commands.command()
+    async def howgay(self, ctx, member: discord.Member=None):
+        if member:
+            return await ctx.send(f'{member.mention} is {random.randint(0, 100)}% gay!')
+        await ctx.send('Please mention someone!')
+
+    @commands.command()
+    async def howlesbian(self, ctx, member: discord.Member=None):
+        if member:
+            return await ctx.send(f'{member.mention} is {random.randint(0, 100)}% lesbian!')
+        await ctx.send('Please mention someone!')
+
+    @commands.command()
+    async def cat(self, ctx):
+        cat = r.get('https://aws.random.cat/meow').json()
+        embed = discord.Embed(
+            title = 'Meow! 🐱',
+            color = discord.Color(9436758)
+        )
+        embed.set_image(url=cat['file'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def dog(self, ctx):
+        dog = r.get('https://random.dog/woof.json').json()
+        embed = discord.Embed(
+            title = 'Woof! 🐶',
+            color = discord.Color(9436758)
+        )
+        embed.set_image(url=dog['url'])
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(fun(client))
